7e0337aca2d421009306a7bbd29e122433faca15
abdullah-hadi <armahfuz47@gmail.com>
2021-06-26 00:15:05 +0600

    -fix_1

--- Flask/app.py
+++ Flask/app.py
@@ -1,30 +1,32 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify, request
 from util import load_file
 import util
 app = Flask(__name__)
 
+
 @app.route('/get_location_name')
-def get_location() :
-    res = jsonify ({
-        'location' : util.get_location()
+def get_location():
+    res = jsonify({
+        'location': util.get_location()
     })
-    res.headers.add('Access-Control-Alow-Origin', '*')
+    res.headers.add('Access-Control-Allow-Origin', '*')
     return res
 
 
-@app.route('/Predict_home_price', methods = ['POST'])
-def price_prediction() :
+@app.route('/Predict_home_price', methods=['POST'])
+def price_prediction():
     total_sqft = float(request.form(['total_sqft']))
     location = request.form(['location'])
     bhk = int(request.form(['bhk']))
     bath = int(request.form(['bath']))
 
-    res = jsonify ({
-        'estimated_price' : util.price_prediction(location, total_sqft, bhk, bath)
+    res = jsonify({
+        'estimated_price': util.price_prediction(location, total_sqft, bhk, bath)
     })
     return res
 
-if __name__ == '__main__' :
+
+if __name__ == '__main__':
     print("Starting python flask server for home price prediction")
     util.load_file()
     app.run(port=5000)
